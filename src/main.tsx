12faa6556e971922269c6435c8cfa4ef3ece928b
cyeju <yeju1019@gmail.com>
2023-12-24 15:34:59 +0900

    feat(colorbox): colorbox 컴포넌트 개발

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { ColorBox } from './components/atoms/ColorBox.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ColorBox>add</ColorBox>
   </React.StrictMode>,
 );
